cfa3469a7b5bcb13164eabcfc96ef75d2fd90b15
diana <@dianayahyaei2005@gmail.com>
2025-08-27 23:18:16 +0330

    logical-oprator

--- js.js
+++ js.js
@@ -1,6 +1,13 @@
-console.log("2" + 5 - 6 + "1");
-console.log("123" < 56);
-console.log("12" * "2");
-console.log(1 + "10" / 2);
-console.log(15 + "5" - 2 * "4" > false);
-// 155-8=147 , false=0 => 147>0 => true
+// console.log("2" + 5 - 6 + "1");
+// console.log("123" < 56);
+// console.log("12" * "2");
+// console.log(1 + "10" / 2);
+// console.log(15 + "5" - 2 * "4" > false);
+// // 155-8=147 , false=0 => 147>0 => true
+// -------------------------------------------------------------
+console.log(true || (false && !not));
+console.log("12" || 0);
+console.log(typeof "12" || 0);
+console.log("12" && 0);
+console.log(null || (false && !"0"));
+// "0"=>true 0 changed to boolean
