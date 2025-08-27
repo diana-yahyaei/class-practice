df885f40c02557fe19cc1f43ac28fcca04b1a6fa
diana <@dianayahyaei2005@gmail.com>
2025-08-27 22:46:27 +0330

    join

---
+++ js.js
@@ -0,0 +1,6 @@
+console.log("2" + 5 - 6 + "1");
+console.log("123" < 56);
+console.log("12" * "2");
+console.log(1 + "10" / 2);
+console.log(15 + "5" - 2 * "4" > false);
+// 155-8=147 , false=0 => 147>0 => true
